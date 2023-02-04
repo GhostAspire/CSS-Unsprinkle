9edd912ae7eb523b32716b54a0c176ac0bce8668
Franklin Okolie <franklinokolie86@gmail.com>
2023-02-04 11:20:03 +0100

    feat: fixed accessibility issues

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -21,7 +21,7 @@ const PhotoGridItem = ({ id, src, alt, tags }) => {
             )} 2x, ${src.replace(".jpg", "@3x.jpg")} 3x`}
           />
 
-          <Image src={src} />
+          <Image alt={alt} src={src} />
         </picture>
       </Anchor>
       <Tags>
